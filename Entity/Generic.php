6535189126e7ffca3de9b9010fbffae108a01519
Thomas Powers <digitalwizard79@gmail.com>
2013-03-10 14:38:49 -0400

    -Imported all files for export to Github

---
+++ Entity/Generic.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Generic Entity Class
+ *
+ * Allows generic entities to be created with any attributes. Useful for
+ * prototyping.
+ *
+ * @author Thomas Powers <digitalwizard79@gmail.com>
+ * @copyright 2012 Thomas Powers
+ * @package PhpORM_Entity
+ */
+class PhpORM_Entity_Generic extends PhpORM_Entity
+{
+    protected $_ignoreMissingProperty = true;
+}
